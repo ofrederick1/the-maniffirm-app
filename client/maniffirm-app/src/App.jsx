00ed563824813c0d8c0e47794b4a038182efad4a
Olivia Frederick <contact@creativelyolivia.com>
2025-06-18 15:56:39 -0400

    work

--- client/maniffirm-app/src/App.jsx
+++ client/maniffirm-app/src/App.jsx
@@ -1,12 +1,37 @@
 import logo from "./assets/manniffirm_logo.png";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Auth/Login";
+
+
 
 function App() {
+
+  // TODO usestat with token
+  // TODO useEffect to savedToken and expiresAt
+  // TODO updateToken
+  // TODO isLoggedIn to toggle between what you see when youre logged in
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+<Router>
+  <div className="App">
+    <header className="App-header">
+   <h1> THE MANIFFIRM APP</h1>
+    </header>
+  </div>
+  <Routes>
+    <Route
+      path="/login"
+      element={<Login />}
+    />
+  </Routes>
+</Router>
+
+
+
+
+
+
+    
   );
 }
 
---
+++ client/maniffirm-app/src/components/Auth/Login.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const Login = (props) => {
+    return (
+        <>
+           <h1> Hello from Login </h1>
+        </>
+    );
+}
+
+export default Login;
